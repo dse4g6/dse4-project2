f62ef3f7c7c289e472eb61b8f6f4436af397e4f5
scada-josh <scada.josh@gmail.com>
2021-08-31 05:40:54 +0700

    change run port

--- volumes/src/main.py
+++ volumes/src/main.py
@@ -54,5 +54,5 @@ def query():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
-    # app.run(host="0.0.0.0", debug=False)
+    # app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", debug=False)
